ddcfe284326f432a04009170352cbc78ba2159c7
junaiddaud <junaiddaud767@gmail.com>
2023-08-23 10:21:06 +0500

    changes

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,18 +1,32 @@
 import React, { useEffect, useState } from "react";
 import Loader from "react-loaders";
 import AnimatedLetters from "../AnimatedLetters";
-import chatgpt from '../../assets/portfolio/chatgpt.PNG'
+import meme from '../../assets/portfolio/meme.png'
+import taxi from '../../assets/portfolio/taxi.png'
 import "./index.scss";
+
 
 
 const Portfolio = () => { 
     const [letterClass, setLetterClass] = useState('text-animate');
-    const [portfolio, setPortfolio] = useState([{
-        name:'Chat GPT-Clone',
-        description:'descripton',
-        image:chatgpt,
+    const [portfolio, setPortfolio] = useState([
+
+    {
+        name:'Meme Editor',
+        description:'A web app where you can make memes from given templates or upload your own template to generate one. Front-end was developed in React with Tailwind for styling',
+        image:meme,
         url:'http://149.28.215.39:7046/chat-gpt'
-    }]);
+    },
+ 
+    {
+        name:'Taxi Booking UI',
+        description:'A Taxi Booking website. Front-end was developed in React with Tailwind for styling',
+        image:taxi,
+        url:'http://149.28.215.39:7046/chat-gpt'
+    }
+
+
+]);
 
     useEffect(() => {
         const timer = setTimeout(() => {
--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -72,7 +72,7 @@ const Sidebar = () => {
       <ul>
         <li>
           <a
-            href="https://www.linkedin.com/in/slobodan-gaji%C4%87-006bb8b8/"
+            href="https://www.linkedin.com/in/junaid-daud-baloch/"
             target="_blank"
             rel="noreferrer"
           >
@@ -85,7 +85,7 @@ const Sidebar = () => {
         </li>
         <li>
           <a
-            href="https://github.com/bobangajicsm"
+            href="https://github.com/junaiddaud"
             target="_blank"
             rel="noreferrer"
           >
@@ -100,7 +100,7 @@ const Sidebar = () => {
           <a
             href="https://www.youtube.com/channel/UCBu5ulO4d-d47lAVybpRTkw"
             rel="noreferrer"
-            target="_blank"
+            target="_blank"   
           >
             <FontAwesomeIcon
               icon={faYoutube}
